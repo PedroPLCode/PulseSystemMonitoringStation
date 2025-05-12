82ce19e9621590f9eb99ca25d01cec007a8db2db
Piotr Gaszczynski <piotrek.gaszczynski@gmail.com>
2025-05-12 20:44:44 +0200

    Change clock jinja template

--- app/static/js/clock.js
+++ app/static/js/clock.js
@@ -1,30 +1,28 @@
-export function startClock(serverTimeString) {
-    const serverTime = new Date(serverTimeString);
+const serverTimeString = document.getElementById('server-time').textContent.trim();
+const serverTime = new Date(serverTimeString);
+
+if (isNaN(serverTime)) {
+  console.error("Invalid server time:", serverTimeString);
+}
+
+let currentTime = new Date(serverTime.getTime());
+
+function updateClock() {
+  const hours = String(currentTime.getHours()).padStart(2, '0');
+  const minutes = String(currentTime.getMinutes()).padStart(2, '0');
+  const seconds = String(currentTime.getSeconds()).padStart(2, '0');
+  document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+
+  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   
-    if (isNaN(serverTime)) {
-      console.error("Invalid server time:", serverTimeString);
-      return;
-    }
+  const date = new Date();
+  const options = { timeZoneName: "short" };
+  const shortTimezone = new Intl.DateTimeFormat("en-US", options).format(date);
   
-    let currentTime = new Date(serverTime.getTime());
-  
-    function updateClock() {
-      const hours = String(currentTime.getHours()).padStart(2, '0');
-      const minutes = String(currentTime.getMinutes()).padStart(2, '0');
-      const seconds = String(currentTime.getSeconds()).padStart(2, '0');
-      document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
-  
-      const date = new Date();
-      const shortTimezone = new Intl.DateTimeFormat("en-US", {
-        timeZoneName: "short",
-      }).format(date);
-  
-      document.getElementById('timezone').textContent = `${shortTimezone}`;
-  
-      currentTime.setSeconds(currentTime.getSeconds() + 1);
-    }
-  
-    updateClock();
-    setInterval(updateClock, 1000);
-  }
-  
+  document.getElementById('timezone').textContent = `${shortTimezone}`;
+
+  currentTime.setSeconds(currentTime.getSeconds() + 1);
+}
+
+updateClock();
+setInterval(updateClock, 1000);
